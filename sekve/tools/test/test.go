0436d3db65da0c6e5181bc9831fee32a1e2c4d4f
Stephen Malone <mail@wirehaiku.org>
2025-11-28 20:13:05 +1000

    Added test.AssertJSON.

--- sekve/tools/test/test.go
+++ sekve/tools/test/test.go
@@ -1,2 +1,20 @@
 // Package test implements unit testing data and functions.
 package test
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// AssertJSON asserts a ResponseRecorder's JSON body is equal to a value.
+func AssertJSON(t *testing.T, w *httptest.ResponseRecorder, code int, want any) {
+	var data any
+	rslt := w.Result()
+	err := json.NewDecoder(rslt.Body).Decode(&data)
+	assert.Equal(t, code, rslt.StatusCode)
+	assert.Equal(t, want, data)
+	assert.NoError(t, err)
+}
--- sekve/tools/test/test_test.go
+++ sekve/tools/test/test_test.go
@@ -1,1 +1,17 @@
 package test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAssertJSON(t *testing.T) {
+	// setup
+	w := httptest.NewRecorder()
+	fmt.Fprintf(w, `"body"`)
+
+	// success
+	AssertJSON(t, w, http.StatusOK, "body")
+}
