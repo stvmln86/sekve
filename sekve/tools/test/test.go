4a3a35dac698f7917ddc0bfabe80bbdfe84609ac
Stephen Malone <mail@wirehaiku.org>
2025-11-28 19:11:53 +1000

    Cleared package.

--- sekve/tools/test/test.go
+++ sekve/tools/test/test.go
@@ -1,14 +1,2 @@
 // Package test implements unit testing data and functions.
 package test
-
-import (
-	"io"
-	"net/http/httptest"
-)
-
-// Response returns the status integer and body string from a ResponseRecorder.
-func Response(w *httptest.ResponseRecorder) (int, string, error) {
-	rslt := w.Result()
-	bytes, err := io.ReadAll(rslt.Body)
-	return rslt.StatusCode, string(bytes), err
-}
